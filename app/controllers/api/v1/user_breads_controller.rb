d47d8a1c3a68f1fe82c0071d33a6117cf756a678
Cory Powell <coryrpowell@gmail.com>
2024-02-07 13:55:58 -0500

    refactor.

--- app/controllers/api/v1/user_breads_controller.rb
+++ app/controllers/api/v1/user_breads_controller.rb
@@ -1,20 +1,17 @@
 class Api::V1::UserBreadsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  before_action :find_user, :find_bread, only: %i[ create update destroy ] 
   def index
 
   end
 # POST /api/v1/user_breads
   def create
-    user = User.find(params[:user_id])
-    bread = Bread.find(params[:bread_id])
-    user_bread = UserBread.create({ user_id: user.id, bread_id: bread.id })
+    user_bread = UserBread.create({ user_id: @user.id, bread_id: @bread.id })
     render json: UserBreadSerializer.new(user_bread).serialized_response, status: 201
   end
 # DELETE /api/v1/user_breads/:user_bread_id
   def destroy
-    user = User.find(params[:user_id])
-    bread = Bread.find(params[:bread_id])
-    user_bread = UserBread.find_by( user_id: user.id, bread_id: bread.id )
+    user_bread = UserBread.find_by( user_id: @user.id, bread_id: @bread.id )
     user_bread.destroy
     render json: { "message": "Successfully removed bread from your BreadBox"}, status: 200
   end
@@ -22,5 +19,13 @@ class Api::V1::UserBreadsController < ApplicationController
   private
   def not_found_response(exception)
     render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)), status: :not_found
+  end
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def find_bread
+   @bread = Bread.find(params[:bread_id])
   end
 end
--- spec/requests/api/v1/user_breads/user_breads_spec.rb
+++ spec/requests/api/v1/user_breads/user_breads_spec.rb
@@ -1,18 +1,19 @@
 require "rails_helper"
 
 RSpec.describe 'UserBreads API endpoints' do
+  before(:each) do
+    @user = User.create!(name: "Bill", email: "bill@bill.com", password: "scienceguy1", password_confirmation: "scienceguy1")
+    @country = Country.create!(name: "Italy", description: "Italian bread is diverse and regionally influenced. Ciabatta, with its irregular holes and crisp crust, is popular. Focaccia, a flatbread often topped with herbs and olive oil, hails from Genoa. Grissini, thin and crunchy breadsticks, are another Italian favorite.")
+    @bread = @country.breads.create!(
+      name: "Ciabatta",
+      description: "Ciabatta is an Italian bread known for its irregular holes, chewy texture, and crisp crust. It's a versatile bread often used for sandwiches or dipping in olive oil.",
+      recipe: "Ingredients: 500g bread flour 10g salt 7g active dry yeast 350ml warm water Instructions: Mix flour and salt. Dissolve yeast in warm water. Combine wet and dry ingredients, knead, and let it rise for 1-2 hours. Shape the dough, rise for 30 minutes, and bake at 425°F (220°C) for 20-25 minutes.",
+      country_id: @country.id
+      )
+  end
   describe "Post /api/v1/user_breads" do
     it "creates a new association between an existing user and bread to store in a user's bread box" do
-      user = User.create!(name: "Bill", email: "bill@bill.com", password: "scienceguy1", password_confirmation: "scienceguy1")
-      country = Country.create!(name: "Italy", description: "Italian bread is diverse and regionally influenced. Ciabatta, with its irregular holes and crisp crust, is popular. Focaccia, a flatbread often topped with herbs and olive oil, hails from Genoa. Grissini, thin and crunchy breadsticks, are another Italian favorite.")
-      bread = country.breads.create!(
-        name: "Ciabatta",
-        description: "Ciabatta is an Italian bread known for its irregular holes, chewy texture, and crisp crust. It's a versatile bread often used for sandwiches or dipping in olive oil.",
-        recipe: "Ingredients: 500g bread flour 10g salt 7g active dry yeast 350ml warm water Instructions: Mix flour and salt. Dissolve yeast in warm water. Combine wet and dry ingredients, knead, and let it rise for 1-2 hours. Shape the dough, rise for 30 minutes, and bake at 425°F (220°C) for 20-25 minutes.",
-        country_id: country.id
-        )
-      
-      post "/api/v1/user_breads", params: { user_id: user.id, bread_id: bread.id }
+      post "/api/v1/user_breads", params: { user_id: @user.id, bread_id: @bread.id }
 
       expect(response).to be_successful
       expect(response.status).to eq(201)
@@ -36,9 +37,9 @@ RSpec.describe 'UserBreads API endpoints' do
       expect(response[:user_attributes]).to have_key(:user_name)
       expect(response[:user_attributes]).to have_key(:email)
       expect(response[:user_attributes][:user_name]).to be_a(String)
-      expect(response[:user_attributes][:user_name]).to eq(user.name)
+      expect(response[:user_attributes][:user_name]).to eq(@user.name)
       expect(response[:user_attributes][:email]).to be_a(String)
-      expect(response[:user_attributes][:email]).to eq(user.email)
+      expect(response[:user_attributes][:email]).to eq(@user.email)
 
       expect(response[:user_bread_attributes]).to be_a(Array)
       expect(response[:user_bread_attributes][0]).to have_key(:bread_attributes)
@@ -49,26 +50,17 @@ RSpec.describe 'UserBreads API endpoints' do
       expect(r).to have_key(:recipe)
       expect(r).to have_key(:bread_country)
       expect(r[:bread_name]).to be_a(String)
-      expect(r[:bread_name]).to eq(bread.name)
+      expect(r[:bread_name]).to eq(@bread.name)
       expect(r[:description]).to be_a(String)
-      expect(r[:description]).to eq(bread.description)
+      expect(r[:description]).to eq(@bread.description)
       expect(r[:recipe]).to be_a(String)
-      expect(r[:recipe]).to eq(bread.recipe)
+      expect(r[:recipe]).to eq(@bread.recipe)
       expect(r[:bread_country]).to be_a(String)
-      expect(r[:bread_country]).to eq(bread.country.name)      
+      expect(r[:bread_country]).to eq(@bread.country.name)      
     end
 
     it "returns an error if passed a user or bread that doesn't exist" do
-      user = User.create!(name: "Bill", email: "bill@bill.com", password: "scienceguy1", password_confirmation: "scienceguy1")
-      country = Country.create!(name: "Italy", description: "Italian bread is diverse and regionally influenced. Ciabatta, with its irregular holes and crisp crust, is popular. Focaccia, a flatbread often topped with herbs and olive oil, hails from Genoa. Grissini, thin and crunchy breadsticks, are another Italian favorite.")
-      bread = country.breads.create!(
-        name: "Ciabatta",
-        description: "Ciabatta is an Italian bread known for its irregular holes, chewy texture, and crisp crust. It's a versatile bread often used for sandwiches or dipping in olive oil.",
-        recipe: "Ingredients: 500g bread flour 10g salt 7g active dry yeast 350ml warm water Instructions: Mix flour and salt. Dissolve yeast in warm water. Combine wet and dry ingredients, knead, and let it rise for 1-2 hours. Shape the dough, rise for 30 minutes, and bake at 425°F (220°C) for 20-25 minutes.",
-        country_id: country.id
-        )
-      
-      post "/api/v1/user_breads", params: { user_id: 490, bread_id: bread.id }
+      post "/api/v1/user_breads", params: { user_id: 490, bread_id: @bread.id }
 
       expect(response).to_not be_successful
       expect(response.status).to eq(404)
@@ -83,19 +75,11 @@ RSpec.describe 'UserBreads API endpoints' do
 
   describe "delete /api/v1/user_breads/:user_bread_id" do
     it "deletes an existing association between an existing user and bread in the user's bread box" do
-      user = User.create!(name: "Bill", email: "bill@bill.com", password: "scienceguy1", password_confirmation: "scienceguy1")
-      country = Country.create!(name: "Italy", description: "Italian bread is diverse and regionally influenced. Ciabatta, with its irregular holes and crisp crust, is popular. Focaccia, a flatbread often topped with herbs and olive oil, hails from Genoa. Grissini, thin and crunchy breadsticks, are another Italian favorite.")
-      bread = country.breads.create!(
-        name: "Ciabatta",
-        description: "Ciabatta is an Italian bread known for its irregular holes, chewy texture, and crisp crust. It's a versatile bread often used for sandwiches or dipping in olive oil.",
-        recipe: "Ingredients: 500g bread flour 10g salt 7g active dry yeast 350ml warm water Instructions: Mix flour and salt. Dissolve yeast in warm water. Combine wet and dry ingredients, knead, and let it rise for 1-2 hours. Shape the dough, rise for 30 minutes, and bake at 425°F (220°C) for 20-25 minutes.",
-        country_id: country.id
-        )
-      user_bread = UserBread.create!( user_id: user.id, bread_id: bread.id )
+      user_bread = UserBread.create!( user_id: @user.id, bread_id: @bread.id )
 
-      expect(user.breads.all.count).to eq(1)
+      expect(@user.breads.all.count).to eq(1)
       
-      delete "/api/v1/user_breads/#{user_bread.id}", params: { user_id: user.id, bread_id: bread.id }
+      delete "/api/v1/user_breads/#{user_bread.id}", params: { user_id: @user.id, bread_id: @bread.id }
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
@@ -106,7 +90,9 @@ RSpec.describe 'UserBreads API endpoints' do
       expect(response_body[:message]).to be_a(String)
       expect(response_body[:message]).to eq("Successfully removed bread from your BreadBox")
 
-      expect(user.breads.all.count).to eq(0)
+      expect(@user.breads.all.count).to eq(0)
+      expect(@user).to be_present
+      expect(@bread).to be_present
     end
   end
 end

1475f415ff4eed407013759754d47dcd5f52ee17
Eric Belongea <eric.belongea@gmail.com>
2024-02-09 15:51:54 -0700

    Small updates and changes

--- app/controllers/api/v1/breads_controller.rb
+++ app/controllers/api/v1/breads_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BreadsController < ApplicationController
+  # GET /api/v1/breads/:bread_name
   def show
     bread = Bread.find_by(name: params[:id].capitalize)
-    # render json: BreadSerializer.new(bread).serializable_hash.to_json
     render json: BreadSerializer.new(bread), status: 200
   end
 end

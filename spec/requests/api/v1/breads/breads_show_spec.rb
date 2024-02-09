5186a8d2f098fc3b0f66151b11235d246e934a3e
Eric Belongea <eric.belongea@gmail.com>
2024-02-08 18:51:50 -0700

    updates

--- spec/requests/api/v1/breads/breads_show_spec.rb
+++ spec/requests/api/v1/breads/breads_show_spec.rb
@@ -3,7 +3,10 @@ require "rails_helper"
 RSpec.describe "Breads API endpoints" do
   describe "/api/v1/breads/:id" do
     it "sends a formatted json response of a specific bread and its attributes" do
-      country = Country.create!(name: "France", description: "France is famous for breads of many forms")
+      c1 = Country.create!(name: "Scotland", 
+                    overview: "Scotland is a country that is part of the United Kingdom, located in the northern part of the island of Great Britain. It is known for its rich history, stunning landscapes, and vibrant cultural heritage. The capital city is Edinburgh, and major cities include Glasgow and Aberdeen. Scotland has a distinct identity with its own legal and education systems.",
+                    culinary: "Scotland's culinary history is characterized by hearty and traditional dishes. Staple foods include haggis, a savory pudding made from sheep's organs, oats, and spices. Scotch whisky, renowned globally, is a significant part of Scottish drinking culture. The cuisine often features locally sourced ingredients such as salmon, venison, and neeps (turnips) and tatties (potatoes).",
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Flag_of_Scotland.svg/1200px-Flag_of_Scotland.svg.png")
 
       bread = country.breads.create!(
         name: "Sourdough",

dbb04ea7b897f587c8d4bd86e4824da066d70673
Eric Belongea <eric.belongea@gmail.com>
2024-02-28 16:09:26 -0700

    Uploads flags for each of countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,75 +27,76 @@ c6 =Country.create!(name: "Italy",
 c7 = Country.create!(name: "China",
                     overview: "China, an ancient civilization in East Asia, boasts a rich cultural history spanning thousands of years. With influential philosophies like Confucianism and Daoism, iconic landmarks such as the Great Wall, and a diverse cuisine emphasizing regional flavors, China's cultural tapestry is characterized by tradition, innovation, and global influence.",
                     culinary: "China's culinary history is diverse and ancient, characterized by a rich tapestry of flavors, cooking techniques, and regional specialties. Influenced by Confucianism and Daoism, Chinese cuisine emphasizes harmony, balance, and the use of fresh, seasonal ingredients. Famous dishes include Peking duck, dim sum, and a variety of noodle and rice-based dishes.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/510px-Flag_of_the_People%27s_Republic_of_China.svg.png")
 c8 = Country.create!(name: "Canada",
                     overview: "Canada, a vast and diverse North American nation, is known for its stunning landscapes, multicultural cities, and friendly people. With French and British influences, Canada embraces a rich cultural mosaic, from the vibrant arts scene to outdoor pursuits like ice hockey. Indigenous traditions further contribute to the country's unique identity." ,
                     culinary: "Canada's culinary history reflects its cultural diversity, drawing inspiration from Indigenous traditions, French and British influences, and the multicultural fabric of the country. Canadian cuisine includes iconic dishes like poutine, butter tarts, and tourtière. The use of locally sourced ingredients, such as maple syrup and seafood, plays a significant role in Canadian gastronomy.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/510px-Flag_of_Canada_%28Pantone%29.svg.png")
 c9 = Country.create!(name: "Egypt",
                     overview: "Egypt, a cradle of ancient civilization in North Africa, is famed for its awe-inspiring pyramids, temples, and the Nile River. With a history dating back to the pharaohs, Egyptian culture showcases a blend of ancient traditions, Islamic influences, and a vibrant contemporary arts scene. The country remains a treasure trove of archaeological wonders.",
                     culinary: "Egyptian cuisine is a blend of ancient traditions and modern influences. Staple foods include flatbreads, falafel, and koshari. With a history dating back to the time of the pharaohs, Egyptian cuisine incorporates flavors from the Mediterranean, Middle East, and North Africa. Herbs and spices, such as cumin and coriander, are integral to the culinary experience.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/510px-Flag_of_Egypt.svg.png")
 c10 = Country.create!(name: "El Salvador",
                     overview: "El Salvador, the smallest country in Central America, boasts a rich cultural heritage influenced by indigenous roots and Spanish colonial history. The nation is known for its lively festivals, delicious pupusas, and a warm sense of community. El Salvador's landscapes, from volcanoes to Pacific beaches, add to its charm.",
                     culinary: "El Salvador's culinary history is influenced by indigenous roots and Spanish colonial heritage. Pupusas, thick corn tortillas stuffed with various fillings, are a national treasure. Traditional Salvadoran cuisine features an array of maize-based dishes, along with a variety of tropical fruits. Seafood, especially along the Pacific coast, is also a prominent part of the cuisine.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Flag_of_El_Salvador.svg/510px-Flag_of_El_Salvador.svg.png")
 c11 = Country.create!(name: "England",
                     overview: "England, part of the United Kingdom, is steeped in history, literature, and iconic landmarks. From Shakespeare to the Beatles, England's cultural impact is vast. With a mix of tradition and modernity, the country embraces diverse art, literature, and a love for football. London, the capital, is a global hub of arts and finance.",
                     culinary: "English cuisine has evolved over centuries, featuring hearty and comforting dishes. Classics like roast beef with Yorkshire pudding, fish and chips, and English breakfast are iconic. The country's culinary traditions showcase the use of locally sourced ingredients, and the pub culture plays a central role in the social fabric, offering a variety of ales and pub fare.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/510px-Flag_of_the_United_Kingdom.svg.png")
 c12 = Country.create!(name: "Ethiopia",
                     overview: "Ethiopia, an ancient land in the Horn of Africa, is a mosaic of cultures and landscapes. Known for its historical sites, including Lalibela's rock-hewn churches, Ethiopia embraces diverse traditions, from ancient Orthodox Christianity to tribal customs. The country's cuisine, highlighted by injera and flavorful spices, adds to its allure.",
                     culinary: "Ethiopian cuisine is characterized by its unique flavors and communal dining experience. Injera, a sourdough flatbread, serves as the staple. Dishes like doro wat (spicy chicken stew) and kitfo (minced meat) showcase the use of aromatic spices. Communal eating from a shared platter, often with injera as a base, is a traditional and social aspect of Ethiopian dining.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Flag_of_Ethiopia.svg/510px-Flag_of_Ethiopia.svg.png")
 c13 = Country.create!(name: "Finland",
                     overview: "Ethiopia, an ancient land in the Horn of Africa, is a mosaic of cultures and landscapes. Known for its historical sites, including Lalibela's rock-hewn churches, Ethiopia embraces diverse traditions, from ancient Orthodox Christianity to tribal customs. The country's cuisine, highlighted by injera and flavorful spices, adds to its allure.",
                     culinary: "Finnish cuisine is influenced by the country's natural resources, with a focus on fish, berries, and game meats. Traditional dishes include karjalanpiirakka (Karelian pasty), kalakukko (fish and pork pie), and hernekeitto (pea soup). The use of fresh, seasonal ingredients and a connection to nature are integral to Finnish culinary traditions.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Finland.svg/510px-Flag_of_Finland.svg.png")
 c14 = Country.create!(name: "Georgia",
                     overview: "Georgia, at the crossroads of Europe and Asia, is a country with a rich cultural heritage. Famed for its hospitality, ancient wine-making traditions, and distinctive alphabet, Georgia's landscapes range from the Caucasus Mountains to the Black Sea. The country's folk dances and polyphonic singing are integral parts of its vibrant culture.",
                     culinary: "Georgian cuisine is known for its bold and flavorful dishes, often featuring walnuts, herbs, and spices. Khachapuri, a cheese-filled bread, and khinkali, dumplings with various fillings, are popular. The country's wine-making traditions, with qvevri (large clay vessels), add a distinct dimension to Georgian gastronomy.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Flag_of_Georgia.svg/510px-Flag_of_Georgia.svg.png")
 c15 = Country.create!(name: "Germany",
                     overview: "Germany, a central European powerhouse, combines a storied history with modern innovation. From the medieval charm of Bavaria to the dynamic art scene in Berlin, Germany offers a diverse cultural experience. Renowned for its precision engineering, classical music, and Oktoberfest, Germany's influence resonates globally.",
                     culinary: "German cuisine is diverse, with regional specialties influenced by the country's geography. Classics like sausages, pretzels, and sauerkraut are well-known. Each region contributes unique dishes, such as Bavarian pretzels, Black Forest cake, and Thuringian bratwurst. Beer culture, with a wide variety of brews, is a significant part of German culinary traditions.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/510px-Flag_of_Germany.svg.png")
 c16 = Country.create!(name: "Greece",
                     overview: "Greece, cradle of Western civilization, is known for its ancient ruins, picturesque islands, and a vibrant Mediterranean lifestyle. With contributions to philosophy, democracy, and mythology, Greece's cultural legacy is profound. The country's cuisine, mythology, and iconic landmarks like the Acropolis attract visitors from around the world.",
                     culinary: "Greek cuisine is a celebration of Mediterranean flavors, featuring olive oil, fresh herbs, and a variety of cheeses. Moussaka, souvlaki, and tzatziki are iconic dishes. The Mediterranean diet, emphasizing vegetables, grains, and seafood, is a cornerstone of Greek gastronomy. Mezedes, a selection of small dishes, showcase the diversity of Greek flavors.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/510px-Flag_of_Greece.svg.png")
 c17 = Country.create!(name: "Iceland",
                     overview: "Iceland, a Nordic island nation, captivates with its otherworldly landscapes, including glaciers, geysers, and waterfalls. Despite its small population, Iceland boasts a rich literary tradition, folklore, and a unique connection to nature. The Icelandic sagas and the Northern Lights contribute to the country's mystical allure.",
                     culinary: "Icelandic cuisine is shaped by the country's unique geography and traditions. With a focus on fish, lamb, and dairy, Icelandic dishes include fermented shark, lamb soup, and skyr (a yogurt-like dairy product). The use of geothermal heat for cooking and baking, such as in rúgbrauð (rye bread), is a distinctive aspect of Icelandic culinary heritage.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/472px-Flag_of_Iceland.svg.png")
 c18 = Country.create!(name: "Indonesia",
                     overview: "Indonesia, an archipelago in Southeast Asia, is a tapestry of diverse cultures, languages, and landscapes. From Bali's vibrant arts scene to Jakarta's modernity, Indonesia embraces a rich heritage influenced by Hindu-Buddhist traditions, Islamic culture, and indigenous customs. The country's cuisine is a flavorful blend of spices and regional specialties.",
                     culinary: "Indonesian cuisine is a vibrant fusion of flavors, influenced by diverse regional cultures. Nasi goreng (fried rice), rendang (spicy beef stew), and satay are popular dishes. The use of aromatic herbs and spices, such as lemongrass and galangal, contributes to the complexity of Indonesian flavors. Street food culture is also integral to the culinary experience.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Flag_of_Indonesia.svg/500px-Flag_of_Indonesia.svg.png")
 c19 = Country.create!(name: "Ireland",
                     overview: "Ireland, an island nation in Western Europe, is known for its lush landscapes, folklore, and friendly locals. With a literary tradition that includes James Joyce and W.B. Yeats, Ireland celebrates its Celtic heritage through music, dance, and festivals. The warmth of Irish hospitality is a hallmark of the country's culture.",
                     culinary: "Irish cuisine reflects the country's agricultural abundance and maritime influences. Traditional dishes include Irish stew, colcannon, and soda bread. Potatoes are a staple, and the Irish pub culture is central to socializing. Irish dairy products, particularly butter and cheese, are renowned for their quality.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Flag_of_Ireland.svg/510px-Flag_of_Ireland.svg.png")
 c20 = Country.create!(name: "Poland",
                     overview: "Poland, situated in Central Europe, has a history marked by resilience and cultural richness. From medieval castles to the historic city of Krakow, Poland's heritage is evident. The country's traditions, including folk art, music, and hearty cuisine, reflect a deep connection to both Eastern and Western European influences.",
                     culinary: "Polish cuisine is characterized by hearty and filling dishes, often featuring potatoes, cabbage, and meats. Pierogi (dumplings), bigos (hunter's stew), and kielbasa (sausage) are staples. The use of spices like caraway and dill adds distinctive flavors to Polish dishes. The country's culinary traditions are deeply rooted in its history and agricultural practices.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Flag_of_Poland.svg/510px-Flag_of_Poland.svg.png")
 c21 = Country.create!(name: "Portugal",
                     overview: "Portugal, on the Iberian Peninsula, is celebrated for its maritime history, explorers like Vasco da Gama, and picturesque landscapes. With a blend of Moorish, African, and European influences, Portugal's cultural diversity is reflected in its music, dance, and cuisine. Fado, a soulful genre of music, is an integral part of Portuguese culture.",
                     culinary: "Portuguese cuisine is a blend of Mediterranean and Atlantic influences. Bacalhau (salted codfish), pastéis de nata (custard tarts), and caldo verde (green soup) are iconic dishes. Portuguese cuisine celebrates fresh seafood, olive oil, and a variety of wines. The pastelarias (pastry shops) showcase the country's sweet delights.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/510px-Flag_of_Portugal.svg.png")
 c22 = Country.create!(name: "Turkey",
                     overview: "Turkey, at the crossroads of Europe and Asia, boasts a rich historical legacy, including the Byzantine and Ottoman empires. Istanbul, with its iconic mosques and bazaars, reflects this cultural fusion. Turkish cuisine, with delights like kebabs and baklava, is renowned globally. The country's traditions include vibrant carpets, ceramics, and the ancient city of Troy.",
                     culinary: "Turkish cuisine is a tapestry of flavors, blending Ottoman, Middle Eastern, and Central Asian influences. Dishes like kebabs, mezes, and baklava showcase the richness of Turkish gastronomy. The use of spices like cumin, sumac, and saffron adds depth to the cuisine. Turkish tea and coffee culture also play a significant role in social gatherings.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/440px-Flag_of_Turkey.svg.png")
 c23 = Country.create!(name: "Ukraine",
                     overview: "Ukraine, the largest country in Europe, is characterized by its diverse landscapes, including the Carpathian Mountains and the Black Sea coast. With a history intertwined with Russia and Europe, Ukraine has a rich cultural heritage. Traditional folk art, literature, and a strong sense of national identity shape the country's cultural landscape.",
                     culinary: "Ukrainian cuisine is hearty and celebrates locally sourced ingredients. Iconic dishes include borscht, pierogi, and holubtsi. Dairy products like sour cream play a significant role. Pickled vegetables, cured meats, and a love for bread contribute to the rich culinary tapestry. Influences from neighboring countries create a unique blend of flavors, reflecting Ukraine's deep connection to the land and cultural identity.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Flag_of_Ukraine.svg/510px-Flag_of_Ukraine.svg.png")
 c24 = Country.create!(name: "United States of America",
                     overview: "The United States, a vast and diverse nation in North America, is known for its cultural melting pot and global influence. From the skyscrapers of New York to the beaches of California, the U.S. offers a dynamic blend of cultures, traditions, and innovations. With contributions to music, film, and technology, the U.S. continues to shape global trends.",
                     culinary: "American cuisine is diverse and influenced by a melting pot of cultures. Regional specialties include Southern barbecue, New England clam chowder, and Tex-Mex dishes. Fast food culture, with iconic items like hamburgers and hot dogs, is also a notable aspect. The U.S. embraces a wide range of international cuisines, reflecting its multicultural society.",
-                    imageUrl: )
+                    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/510px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png")
+
 # Scotland
 c1.breads << Bread.create!(name: "Scotch Broth Bread",
                           description: "Inspired by traditional Scotch Broth soup, this hearty bread is infused with flavors of lamb, barley, and root vegetables. A rustic and comforting choice.",
